222d9615e314ea98fbae439abc7111c987e2a42b
Andrelcorcettidev <andre_luiz_corcetti@hotmail.com>
2023-07-31 11:43:10 -0300

    Exercícios de JS

---
+++ intensivao.js
@@ -0,0 +1,69 @@
+// // NÚMERO 01
+alert('Hello word!')
+
+
+// // NÚMERO 02
+let num1 = prompt('Digite o primeiro número para a soma:')
+let num2 = prompt('Agora digite o segundo número:')
+soma = Number(num1) + Number(num2)
+alert(soma)
+
+// // NÚMERO 03
+let isNumber = 5
+
+isNumber = Number(isNumber)
+
+if(typeof isNumber == 'number'){
+    alert('É um número')
+} else{
+    alert('Não é um número')
+}
+
+// NÚMERO 04
+
+alert('André')
+
+if(typeof box == 'string'){
+    alert('é uma string')
+} else {
+    alert('Não é uma string')
+}
+
+// NÚMERO 05
+
+let bool = true
+
+if(typeof bool == 'boolean'){
+    alert('É um bool')
+} else{
+    alert('Não é um bool')
+}
+
+// NÚMERO 09
+
+let num1 = prompt('Digite o primeiro número')
+let num2 = prompt('Digite o segundo número')
+
+let op = Number(num1) + Number(num2)
+
+if(op % 2 == 0){
+    alert('É um número par')
+} else{
+    alert('Não é par')
+}
+
+
+// NÚMERO 10
+
+let num1 = prompt('Digite o primeiro número')
+let num2 = prompt('Digite o segundo número')
+
+let op = Number(num1) + Number(num2)
+
+if(op % 2 != 0){
+    alert('É um número impar')
+} else{
+    alert('É um número não impar')
+}
+
+
